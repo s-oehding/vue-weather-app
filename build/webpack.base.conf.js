fe92d74784f29ccfb9cd1a561969ae2c645f3261
Sören Oehding <soeren.oehding@gmail.com>
2016-12-09 12:11:08 +0100

    Proof of concept

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,109 @@
+var path = require('path')
+var config = require('../config')
+var utils = require('./utils')
+var projectRoot = path.resolve(__dirname, '../')
+var MODULE_DIR = path.resolve(__dirname, '../node_modules')
+var env = process.env.NODE_ENV
+// check env & config/index.js to decide weither to enable CSS Sourcemaps for the
+// various preprocessor loaders added to vue-loader at the end of this file
+var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
+var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
+var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  externals: {
+    'TweenLite': 'TweenLite'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.vue'],
+    fallback: [path.join(__dirname, '../node_modules')],
+    alias: {
+      'vue$': 'vue/dist/vue',
+      'src': path.resolve(__dirname, '../src'),
+      'assets': path.resolve(__dirname, '../src/assets'),
+      'components': path.resolve(__dirname, '../src/components'),
+      'TweenLite': path.resolve(MODULE_DIR, 'gsap/src/minified/TweenLite.min.js'),
+      'TweenMax': path.resolve(MODULE_DIR, 'gsap/src/minified/TweenMax.min.js'),
+      'TimelineLite': path.resolve(MODULE_DIR, 'gsap/src/minified/TimelineLite.min.js'),
+      'TimelineMax': path.resolve(MODULE_DIR, 'gsap/src/minified/TimelineMax.min.js'),
+      'EasePack': path.resolve(MODULE_DIR, 'gsap/src/minified/easing/EasePack.min.js')
+    }
+  },
+  resolveLoader: {
+    fallback: [path.join(__dirname, '../node_modules')]
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.vue$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+  node: {
+    fs: 'empty'
+  },
+  vue: {
+    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['last 2 versions']
+      })
+    ]
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <div class="map-wrapper">
+      <gmap v-if="ready" :api-key="gmapKey" :lat.sync="location.lat" :lng.sync="location.lng" :address.sync="address" :zoom.sync="zoom" v-on:resetMap="resetMap()"></gmap>
+    </div>
+    <div class="controls-wrapper">
+      <nav class="navbar row">
+        <div class="col-2">
+          <label for="lat">Latitude</label>
+          <input type="text" name="lat" class="form-input" v-model.lazy="location.lat" placeholder="latitude">
+        </div>
+        <div class="col-2">
+          <label for="lng">Longitude</label>
+          <input type="text" name="lng" class="form-input" v-model.lazy="location.lng" placeholder="longitude">
+        </div>
+        <div class="col-2">
+          <label for="geolocation">Geolocation</label>
+          <a class="btn" v-on:click="getGeolocation()">Get my Position</a>
+        </div>
+        <div class="col-2">
+          <label for="address">Geocode Address</label>
+          <input type="text" name="address" class="form-input" v-model.lazy="address" placeholder="Hamburg DE">
+        </div>
+        <div class="col-2">
+          <label for="zoom">Zoom</label>
+          <input v-model="zoom" class="form-input" type="range" id="zoom" min="3" max="18" step="1">
+        </div> 
+        <div class="col-2">
+          <label for="lng">Reset to Marker</label>
+          <a class="btn" v-on:click="resetMap()">Center Map</a>
+        </div> 
+      </nav>
+    </div>
+    <div class="sidebar-wrapper">
+      <weather-data v-if="ready" :lat="location.lat" :lng="location.lng"></weather-data>
+    </div>
+  </div>
+</template>
+
+<script>
+import WeatherData from './components/WeatherData'
+import Gmap from './components/Gmap'
+
+// import WeatherMap from './components/WeatherMap'
+
+export default {
+  name: 'app',
+  components: {
+    WeatherData, Gmap
+  },
+  data () {
+    return {
+      gmapKey: 'AIzaSyCZtDHCa5264C_wus713s_is44ACsEtqmY',
+      location: {
+        lat: null,
+        lng: null
+      },
+      address: '',
+      zoom: 10,
+      ready: false
+    }
+  },
+  mounted () {
+    this.getGeolocation()
+  },
+  methods: {
+    getGeolocation () {
+      if ('geolocation' in navigator) {
+        var gl = navigator.geolocation
+        gl.getCurrentPosition(function (position) {
+          this.location.lng = position.coords.longitude
+          this.location.lat = position.coords.latitude
+          // console.log('GEOLOCATION', position.coords)
+          this.ready = true
+        }.bind(this))
+      }
+    },
+    setReady () {
+      this.ready = true
+    },
+    resetMap () {
+      this.$emit('resetMap')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'assets/scss/app.scss'
+</style>
